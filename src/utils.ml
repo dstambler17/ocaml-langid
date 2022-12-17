f61811bb9131a17290efef40c010dbcd39bccaab
dstambler17 <dstambler17@gmail.com>
2022-12-17 14:24:27 -0500

    Added base sampler logic w/ requests

--- src/utils.ml
+++ src/utils.ml
@@ -9,3 +9,14 @@ let read_file_input (file_path: string): string list =
   In_channel.close file;
   strings
 
+(*
+  
+ Taken from Assignment 4 of the JHU functional course
+ Allows for us to pass in a random module to file
+*)
+module type Randomness = sig
+  (*
+    Given a maximum integer value, return a pseudorandom integer from 0 (inclusive) to this value (exclusive).
+  *)
+  val int : int -> int
+end
