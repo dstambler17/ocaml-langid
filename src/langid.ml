eeca856475f771fec1f75657f46ceaa1ca291ce3
gurion <gurion@gmail.com>
2022-12-17 20:01:23 -0500

    print instructions bug fix

--- src/langid.ml
+++ src/langid.ml
@@ -3,35 +3,54 @@
 [@@@ocaml.warning "-26"]
 
 open Core
+open Printf
+open Stdio
 module M = Models
 module G = Game
 module CLI = Minicli.CLI
 
-let arguments_print =
-  Printf.printf
-    "\n\
-     Arguments:\n\
-     \t-mode:\n\
-     \t\tgame - play a langid guessing game against the model\n\
-     \t\teval - classify a string of text\n\
-     \t-top_n:\n\
-     \t\tnumber of predicted languages to output\n\
-     \t-input;-i:\n\
-     \t\tstring to be classified if eval mode is used\n\n\
-     %!"
+let help_print =
+  "\n\
+   Example usage:\n\
+   \t./langid.exe -mode <string> -top_n <int> -input <string> [-h;-help]\n\n\
+   Arguments:\n\
+   \t-mode:\n\
+   \t\tgame - Play a langid guessing game against the model\n\
+   \t\teval - Classify a string of text\n\
+   \t-top_n:\n\
+   \t\tNumber of predicted languages to output\n\
+   \t-input;-i:\n\
+   \t\tString to be classified if eval mode is used\n\
+   \t\tMake sure your whole string is in quotes!!\n\n"
 
-let example_print =
-  Printf.printf
-    "\n\
-     Example usage:\n\
-     \t./langid.exe -mode <string> -top_n <int> -input <string> [-h;-help]\n\n\
-     %!"
+let rec play_game () =
+  printf "Game line\n";
+  let input =
+    Out_channel.(flush stdout);
+    In_channel.(input_line_exn stdin)
+  in
+  if String.( = ) input "" then play_game ()
+  else (
+    print_endline "";
+    exit 1)
+
+let evaluate input_text () =
+  let classified = input_text |> Models.classify |> List.hd_exn in
+  let language = Tuple2.get1 classified in
+  let likelihood = Tuple2.get2 classified *. 100. in
+  printf
+    "You input:\n\
+     %s\n\n\
+     This is written in %s.\n\
+     We say this with %.2f%% likelihood.\n\n\
+     Thanks for using langid! :)%!" input_text language likelihood;
+  print_endline "";
+  Out_channel.(flush stdout)
 
 let main () =
   let argc, args = CLI.init () in
   if argc = 1 then (
-    example_print;
-    arguments_print;
+    printf "%s" help_print;
     exit 1);
   let mode = CLI.get_string_def [ "-mode" ] args "eval" in
   let input_text =
@@ -44,9 +63,14 @@ let main () =
   CLI.finalize ();
   match help with
   | true ->
-      example_print;
-      arguments_print
-  | false -> ()
+      printf "%s" help_print;
+      exit 1
+  | false -> (
+      match mode with
+      | "game" -> play_game ()
+      | "eval" -> evaluate input_text ()
+      | _ ->
+          printf "Bad mode parameter given:\n\t%s" mode;
+          exit 1)
 
-let run_game () = main ()
 let () = main ()
